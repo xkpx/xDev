0b179c637983de1b2142167d7bfca5a521584d76
xkpx64 <xkpx64@mail.ru>
2022-07-06 15:14:33 +0300

    *

--- xSETUP.cjs
+++ xSETUP.cjs
@@ -1,20 +1,31 @@
 // ===================================================
 // https://pnpm.io/npmrc 
 // https://docs.npmjs.com/cli/v8/using-npm/config/
-// =================================================== 05.07.2022 
+// =================================================== 06.07.2022 
 const {execSync} = require('child_process');    
 	execSync("set NODE_ENV=production", { stdio: "inherit" }); 
 	execSync("pnpm env use --global latest", { stdio: "inherit" });  // nightly=19 nightly=18.4 	
-	execSync("pnpm install -P --no-optional @sveltejs/kit@next vite@beta svelte svelte-preprocess @sveltejs/adapter-node@next svelte-adapter-deno javascript-obfuscator rollup-obfuscator express@next dotenv", { cwd: process.cwd(), detached: false, stdio: "inherit" });
+	execSync("pnpm install -P --no-optional \
+				@sveltejs/kit@next \
+				vite@beta \
+				svelte \
+				svelte-preprocess \
+				@sveltejs/adapter-node@next \
+				svelte-adapter-deno \
+				javascript-obfuscator \
+				rollup-obfuscator \
+				express@next \
+				dotenv ", { cwd: process.cwd(), detached: false, stdio: "inherit" });
 	//execSync("pnpm install -P --no-optional svelte @sveltejs/kit@next vite@beta", { cwd: process.cwd(), detached: false, stdio: "inherit" });
 	//execSync("pnpm install -P --no-optional svelte-preprocess svelte-check", { cwd: process.cwd(), detached: false, stdio: "inherit" }); // tslib2.4 typescript@next4.8?
 	//execSync("pnpm install -P --no-optional express@next dotenv @sveltejs/adapter-node@next svelte-adapter-deno javascript-obfuscator rollup-obfuscator", { cwd: process.cwd(), detached: false, stdio: "inherit" });
+	execSync("pnpm prune --no-optional --prod");
 	execSync("pnpm store prune", 						{ cwd: process.cwd(), detached: false, stdio: "inherit" }); // Removes store unnecessary packages.
 	execSync("pnpm install --fix-lockfile", 			{ cwd: process.cwd(), detached: false, stdio: "inherit" });
 
-/*	execSync("pnpm config set hoist true --userconfig .npmrc"); 
+/*
 	execSync("pnpm uninstall node-pre-gyp -r")				
 	execSync("pnpm install @mapbox/node-pre-gyp esbuild-windows-64 chokidar");	// pnp+symlink/false
-	pnpm prune --no-optional --prod
+	execSync("pnpm prune --no-optional --prod");
 	execSync("pnpm up --latest --interactive", 			{ cwd: process.cwd(), detached: false, stdio: "inherit" });
 	execSync("pnpm outdated", 							{ cwd: process.cwd(), detached: false, stdio: "inherit" }); */
